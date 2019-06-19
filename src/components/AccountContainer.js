878ea4d23bcca9bf22f94bfbc573f2fabf39a999
Sarah Elzinga <sevans924@gmail.com>
2019-06-19 10:35:44 -0500

    rendering info on page from fetch

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -7,11 +7,23 @@ class AccountContainer extends Component {
 
   constructor() {
     super()
-
+    this.state = {
+      transactions: []
+    }
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
 
+  }
+
+  componentDidMount(){
+    fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+    .then(resp => resp.json())
+    .then(transactionData => {
+      this.setState({
+        transactions: transactionData
+      })
+    })
   }
 
   handleChange(event) {
@@ -23,7 +35,7 @@ class AccountContainer extends Component {
     return (
       <div>
         <Search />
-        <TransactionsList />
+        <TransactionsList trans={this.state.transactions}/>
       </div>
     )
   }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,38 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
+class TransactionsList extends Component {
 
-  return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
-          <th>
-            <h3 className="ui center aligned header">
-              Posted At
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Description
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Category
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Amount
-            </h3>
-          </th>
-        </tr>
+  
 
-        {"... your code here..."}
+  
 
-      </tbody>
-    </table>
-  )
+  
+
+
+  render(){
+    const renderTransaction = this.props.trans.map(transy => {
+     return (<Transaction 
+      posted={transy.posted_at} 
+      description={transy.description}
+      category={transy.category}
+      amount={transy.amount}/>)
+    })
+
+    return (
+      <table className="ui celled striped padded table">
+        <tbody>
+          <tr>
+            <th>
+              <h3 className="ui center aligned header">
+                Posted At
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Description
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Category
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Amount
+              </h3>
+            </th>
+          </tr>
+
+          {renderTransaction}
+
+        </tbody>
+      </table>
+    )
+  }
+
 }
 
 export default TransactionsList
